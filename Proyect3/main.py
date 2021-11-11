fd4ee0c0a9727c5d71a0b9c24d4bfecf9b3955b6
diegoalvarez17 <diegoalvarez1237@hotmail.com>
2021-11-10 20:32:36 -0600

    start proyect 3

--- Proyect3/main.py
+++ Proyect3/main.py
@@ -1,27 +1,73 @@
 import pygame
+import numpy
 from OpenGL.GL import *
+from OpenGL.GL.shaders import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((500,500), pygame.OPENGL | pygame.DOUBLEBUF) 
 
-x = 100
-y = 100
+glClearColor(0.5, 0.2, 0.5, 1.0)
 
+vertex_shader = """
+#version 460
+
+layout (location = 0) in vec3 position;
+
+void main()
+{
+    gl_Position = vec4(position.xyz, 1);
+}
+"""
+
+fragment_shader = """
+#version 460
+
+layout (location = 0) out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);
+}
+"""
+
+cvs = compileShader(vertex_shader, GL_VERTEX_SHADER)
+cfs = compileShader(fragment_shader, GL_FRAGMENT_SHADER)
+
+shader = compileProgram(cvs, cfs)
+
+vertex_data = numpy.array([
+ -1, -1, 0,
+  1, -1, 0,
+  0,  1, 0
+], dtype=numpy.float32)
+
+vertex_buffer_object = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object)
+glBufferData(GL_ARRAY_BUFFER, vertex_data.nbytes, vertex_data, GL_STATIC_DRAW)
+
+vertex_array_object = glGenVertexArrays(1)
+glBindVertexArray(vertex_array_object)
+
+glVertexAttribPointer(
+    0, # location
+    3, # size
+    GL_FLOAT, # type
+    GL_FALSE, # normalize
+    4 * 3, # stride
+    ctypes.c_void_p(0)
+)
+glEnableVertexAttribArray(0)
+
+running = True
 while True:
-    screen.set_at((x,y), (255,255,255))
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glUseProgram(shader)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
 
     pygame.display.flip()
-
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             running = False
-        if (event.type == pygame.KEYDOWN):
-            if (event.key == pygame.K_w):
-                y += 1
-            if (event.key == pygame.K_a):
-                x -= 1
-            if (event.key == pygame.K_s):
-                y -= 1
-            if (event.key == pygame.K_d):
-                x += 1
+        
